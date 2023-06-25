cac9db4f9e62c148d8b4864b9c8f7742ac471eaf
agu chidimma maryfiona <aguchidimmamaryfiona@gmail.com>
2023-06-25 14:13:06 +0100

    removed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,25 +6,26 @@ import { faTable } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="contact-section">
-        <h3>Want to Get in touch?</h3>
-        <h4>Send me a mail 📩</h4>
-        <ul className="social-icons">
-          <li><a href="/"><FontAwesomeIcon icon={faEnvelope} /></a></li>
-          <li><a href="/"><FontAwesomeIcon icon={faLinkedin} /></a></li>
-          <li><a href="/"><FontAwesomeIcon icon={faTwitter} /></a></li>
-          <li><a href="/"><FontAwesomeIcon icon={faInstagram} /></a></li>
-          <li><a href="/"><FontAwesomeIcon icon={faBehance} /></a></li>
-          <li><a href="/"><FontAwesomeIcon icon={faDribbble} /></a></li>
-          <li><a href="/"><FontAwesomeIcon icon={faFacebook} /></a></li>
-          <li><a href="/"><FontAwesomeIcon icon={faFigma} /></a></li>
-          <li><a href="/"><FontAwesomeIcon icon={faMedium} /></a></li>
-          <li><a href="/"><FontAwesomeIcon icon={faTable} /></a></li>
-        </ul>
-        <h4>2023 (c) All Rights Reserved. Maginations</h4>
-      </div>
-    </footer>
+    <></>
+    // <footer>
+    //   <div className="contact-section">
+    //     <h3>Want to Get in touch?</h3>
+    //     <h4>Send me a mail 📩</h4>
+    //     <ul className="social-icons">
+    //       <li><a href="/"><FontAwesomeIcon icon={faEnvelope} /></a></li>
+    //       <li><a href="/"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+    //       <li><a href="/"><FontAwesomeIcon icon={faTwitter} /></a></li>
+    //       <li><a href="/"><FontAwesomeIcon icon={faInstagram} /></a></li>
+    //       <li><a href="/"><FontAwesomeIcon icon={faBehance} /></a></li>
+    //       <li><a href="/"><FontAwesomeIcon icon={faDribbble} /></a></li>
+    //       <li><a href="/"><FontAwesomeIcon icon={faFacebook} /></a></li>
+    //       <li><a href="/"><FontAwesomeIcon icon={faFigma} /></a></li>
+    //       <li><a href="/"><FontAwesomeIcon icon={faMedium} /></a></li>
+    //       <li><a href="/"><FontAwesomeIcon icon={faTable} /></a></li>
+    //     </ul>
+    //     <h4>2023 (c) All Rights Reserved. Maginations</h4>
+    //   </div>
+    // </footer>
   );
 };
 
