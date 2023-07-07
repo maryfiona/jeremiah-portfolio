347c789fd1140efe815d61bef61cd58b4655906b
agu chidimma maryfiona <aguchidimmamaryfiona@gmail.com>
2023-07-07 23:52:22 +0100

    done for 2day

--- src/components/About.js
+++ src/components/About.js
@@ -10,19 +10,24 @@ const About = () =>{
         <React.Fragment>
    <Navbar />
 
-   <div className="text-success">
+   <div className="text">
     About us
    </div>
    <div className="container"> 
-                <div className="pa">Hello, 👋</div>
-                <h1 className="p">i’m Olorunfemi Damilare  <br />
-                    Product & brand Designer <br />
-                    and Design instructor</h1>
-                <p className="pp">I’m a Product & brand  designer base in Lagos NG, Passionate about Creativity  <br />
-                    in Product with over  4years of Experience in experiential now, <br />
-                    brand strategy and UI/UX design. I've helped build up upcoming brands <br />
-                    and create meaningful and memorable user-centered  solutions that <br />
-                    develop intuition between technology and people.</p>
+                
+                <h1>i’m Olorunfemi Damilare  <br />
+                    </h1>
+                <p>Hello! I'm Olorunfemi Damilare, passionate about creativity <br />
+on product and brand with over 4years of experience in <br /> experiential now, brand strategy, and UX design. I've helped <br /> 
+ build up brands and create meaningful and memorable user <br /> experiences with impactful  and visual design solutions.<br /> 
+                 I currently work with an agency of Government on  <br />
+                 skill acquisition, and my job is to Train women in tech skills, <br />
+                  especially on the desktop package, design, and UX UI design. <br />
+ I often work as a UI designer, but sometimes I am that guy  <br />
+  who likes to design and  get the best out of the product while <br />
+   ensuring the user makes an effort to take the following action. <br />  So yeah call me a remote UXUI designer.
+ <br /> I love what I do, and  <br /> I'm not scared  to roll up my sleeves when it comes to doing  <br /> 
+ the digging😊.</p>
 
             </div>
    
