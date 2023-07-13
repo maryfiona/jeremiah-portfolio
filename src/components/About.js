3af516ce0da7d02479ce1adb3ef12d968c83d5e3
agu chidimma maryfiona <aguchidimmamaryfiona@gmail.com>
2023-07-13 16:43:20 +0100

    still working under about.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,42 +1,52 @@
 import React from "react";
 import './About.css';
 import Navbar from "./Navbar";
+import profile1 from '../Assets/profile1.png';
 
 
 
 
-const About = () =>{
-    return(
+const About = () => {
+    return (
         <React.Fragment>
-   <Navbar />
+            <Navbar />
 
-   <div className="text">
-    About us
-   </div>
-   <div className="container"> 
-                
+            <div className="text">
+                About us
+            </div>
+            <div className="container">
+
                 <h1>i’m Olorunfemi Damilare  <br />
-                    </h1>
+                </h1>
                 <p>Hello! I'm Olorunfemi Damilare, passionate about creativity <br />
-on product and brand with over 4years of experience in <br /> experiential now, brand strategy, and UX design. I've helped <br /> 
- build up brands and create meaningful and memorable user <br /> experiences with impactful  and visual design solutions.<br /> 
-                 I currently work with an agency of Government on  <br />
-                 skill acquisition, and my job is to Train women in tech skills, <br />
-                  especially on the desktop package, design, and UX UI design. <br />
- I often work as a UI designer, but sometimes I am that guy  <br />
-  who likes to design and  get the best out of the product while <br />
-   ensuring the user makes an effort to take the following action. <br />  So yeah call me a remote UXUI designer.
- <br /> I love what I do, and  <br /> I'm not scared  to roll up my sleeves when it comes to doing  <br /> 
- the digging😊.</p>
+                    on product and brand with over 4years of experience in <br /> experiential now, brand strategy, and UX design. I've helped <br />
+                    build up brands and create meaningful and memorable user <br /> experiences with impactful  and visual design solutions.<br />
+                    I currently work with an agency of Government on  <br />
+                    skill acquisition, and my job is to Train women in tech skills, <br />
+                    especially on the desktop package, design, and UX UI design. <br />
+                    I often work as a UI designer, but sometimes I am that guy  <br />
+                    who likes to design and  get the best out of the product while <br />
+                    ensuring the user makes an effort to take the following action. <br />  So yeah call me a remote UXUI designer.
+                    <br /> I love what I do, and  <br /> I'm
+                    not scared  to roll up my sleeves when it comes to doing  <br />
+                    the digging😊.</p>
+                <img src={profile1} alt="experience" />
 
             </div>
-            <div>MY EXPERIENCE</div>
-            
-   
+            <div className="container">
+                <div>MY EXPERIENCE</div>
+                <div>
+                    1. Product designer (Web3.0)     <p>Nov 2022 Till date</p>
+                    <p>caption lion</p>  <p>San Francisco</p>
+                </div>
+
+            </div>
+
+
 
         </React.Fragment>
     )
-    
+
 
 };
 export default About;
